6b6100d6da5998a13377d880c9215591bc56fbff
John Hopper <john.hopper@rackspace.com>
2013-08-02 15:21:21 -0500

    documentation

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def ez_install(package):
 
 setup(
     name='pynsive',
-    version='0.1.1',
+    version='0.1.2',
     description=("""A Python plugin and introspection library prounounced,
 "pensive." This library allows a developer to introduce plugins via PEP 302
 and allows for class searching via type and inheritance introspections.
@@ -36,7 +36,9 @@ and allows for class searching via type and inheritance introspections.
     license='Apache 2.0',
     classifiers=[
         'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
+        'Environment :: Plugins',
         'Programming Language :: Python',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: 2.7',
